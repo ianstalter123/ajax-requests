4d8aee90e6c3559c84affb7dabfa45444e04d329
ianstalter123 <istalter66@gmail.com>
2015-06-01 22:39:54 -0700

    second

--- github.js
+++ github.js
@@ -1,3 +1,50 @@
 $(function() {
-// TODO: You code goes here.
+console.log("script is live");
+
+var input = document.querySelector('input')
+	
+
+input.addEventListener('keyup',function(e) {
+
+
+var text = $('input').val();
+var test = $('#username').text();
+	if(e.which === 13)
+	{
+
+	 var str = "https://api.github.com/search/users?q=" + text;
+	 $.get( str,
+     function( data ) {
+     var string = JSON.stringify(data);
+     var result = JSON.parse(string);
+    
+     $('#username').html(result.items[0].login);
+     console.log($('#username').text())
+
+     $('#profilePhoto').attr("src", result.items[0].avatar_url);
+     });
+     
+    
+     var repo = "https://api.github.com/search/repositories?q=user%3A"+test;
+     console.log(repo);
+      $.get( repo,
+     function( data ) {
+     var string = JSON.stringify(data);
+     var result = JSON.parse(string);
+
+console.log(result);
+     //$('#repositories').html(result.items[0]);
+   
+    });
+	}
+
+})
+	
+
+
+
+
+
+
 });
+
